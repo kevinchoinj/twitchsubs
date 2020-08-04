d681ae5a4ad2846c7637b9e88d42f87085855a68
unknown <kevinchoinj@yahoo.com>
2020-08-04 09:52:48 -0500

    new data like messages and split up est subs and rec subs

--- client/src/reducers/data.js
+++ client/src/reducers/data.js
@@ -13,8 +13,8 @@ export default(state=DEFAULT_STATE, payload)=>
     return state = {
       ...state,
       data: payload?.data?.map((data) => {
-        if (data.subs) {
-          data.percentage_gifted = ((data.gifted_tier1_subs + data.gifted_tier2_subs + data.gifted_tier3_subs)/data.subs)*100;
+        if (data.recorded_subs) {
+          data.percentage_gifted = ((data.gifted_tier1_subs + data.gifted_tier2_subs + data.gifted_tier3_subs)/data.recorded_subs)*100;
         }
         else {
           data.percentage_gifted = 0;
